144ebdd66a05cc74b2ef51b7d1cbcb70a869df3c
shukubota <kubotashu0421@gmail.com>
2021-05-30 21:57:32 +0900

    mutation例追加

--- src/recipes/recipes.resolver.ts
+++ src/recipes/recipes.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { Recipe } from './recipe';
 
 const recipeTable = [
@@ -16,10 +16,29 @@ const recipeTable = [
   },
 ];
 
+const updatedRecipe = {
+  id: '1',
+  title: 'title1-new',
+};
+
 @Resolver(() => Recipe)
 export class RecipesResolver {
   @Query(() => [Recipe])
   async recipes(): Promise<Recipe[]> {
     return recipeTable;
   }
+
+  @Mutation(() => Recipe)
+  async updateRecipe(@Args({ name: 'title', type: () => String }) title: string): Promise<Recipe> {
+    console.log(title);
+    return updatedRecipe;
+  }
+
+  // playground↓
+  // mutation {
+  //   updateRecipe(title: "new") {
+  //     id
+  //     title
+  //   }
+  // }
 }
