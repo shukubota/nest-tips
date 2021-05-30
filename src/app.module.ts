c68bf43dd47c26d1bd98a3be36147d999d9cb296
shukubota <kubotashu0421@gmail.com>
2021-05-30 18:19:23 +0900

    graphqlModule追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+})
+class _AppModule {}
+
+@Module({
+  imports: [
+    // _AppModule,
+    GraphQLModule.forRoot({
+      // include: [RecipesModule],
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      debug: true,
+      playground: true,
+    }),
+    RecipesModule,
+  ],
+  controllers: [],
+  providers: [],
+  // controllers: [AppController],
+  // providers: [AppService],
 })
 export class AppModule {}
